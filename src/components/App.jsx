25c9ba50ebc8f636a47aefbd9fda8e5d095e4684
NataliiaYa <118573896+NataliiaYa@users.noreply.github.com>
2023-07-14 01:14:57 +0300

    process

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+import { featchImages } from '../services/api';
+
+
+export class App extends Component{
+  state = {
+    query: "",
+    images: [],
+    page: 1,
+    totalHits: 0,
+    isLoading: false,
+    isEmpty: false,
+    isVisible: false,
+    error: null,
+    modalImg: null
+  };
+
+componentDidUpdate(_, prevState) {
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      this.getImages(query, page);
+    }
+  }
+  getImages = async () => {
+    this.setState({ isLoading: true });
+    try {
+      const { hits, totalHits } = await fetchImages(
+        this.state.query,
+        this.state.page
+      );
+
+      if (this.state.images.length === 0) {
+        this.setState({ isEmpty: true });
+      }
+
+      this.setState((prevState) => ({
+        images: [...prevState.images, ...hits],
+        totalImages: totalHits
+      }));
+    } catch (error) {
+      this.setState({ error: error.message });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
